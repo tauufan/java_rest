11aeb3768b88cf6285ab8df7820e31a6589b6dfe
taufanyanakbar <taufan.yan@gmail.com>
2022-03-02 23:49:31 +0700

    put

--- src/main/java/com/bjb/api/repository/CimRepository.java
+++ src/main/java/com/bjb/api/repository/CimRepository.java
@@ -1,6 +1,8 @@
 package com.bjb.api.repository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +10,6 @@ import com.bjb.api.model.Cim;
 
 public interface CimRepository extends JpaRepository<Cim, Long>{
 	public Cim findByCif(String cif);
+	public List<Cim> findByNomorid(String nomorid);
 	public List<Cim> findByCif_(String cif);
 }
